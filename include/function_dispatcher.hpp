5196a1be3600cb01cb2c0fbf7dae00abf206305f
Bastien Souchu <bastien.souchu@volvocars.com>
2024-11-19 15:05:28 +0100

    Fix leaks and UB

--- include/function_dispatcher.hpp
+++ include/function_dispatcher.hpp
@@ -5,6 +5,7 @@
 #include <unordered_map>
 #include <typeindex>
 #include <iostream>
+#include <vector>
 
 class function_dispatcher_bad_function_call : public std::bad_function_call
 {
@@ -14,6 +15,40 @@ public:
 
 private:
     std::string message_;
+};
+
+class IType
+{
+public:
+    virtual ~IType() = default;
+
+    virtual void CastAndDelete(void *) const = 0;
+    virtual std::type_index GetTypeIndex() const = 0;
+};
+
+template <typename FuncSignature>
+class Type : public IType
+{
+public:
+    Type() : type_index_(typeid(FuncSignature))
+    {
+    }
+    void CastAndDelete(void *ptr) const override
+    {
+        CastAndDelete_impl(ptr, std::make_index_sequence<std::tuple_size<typename FuncSignature::args_t>::value>{});
+    }
+    std::type_index GetTypeIndex() const override
+    {
+        return type_index_;
+    }
+
+private:
+    template <std::size_t... I>
+    void CastAndDelete_impl(void *ptr, std::index_sequence<I...>) const
+    {
+        delete static_cast<std::function<typename FuncSignature::return_t(std::tuple_element_t<I, typename FuncSignature::args_t>...)> *>(ptr);
+    }
+    std::type_index type_index_;
 };
 
 class FunctionDispatcher
@@ -33,10 +68,9 @@ public:
     FunctionDispatcher(const FunctionDispatcher &other) = delete;
     ~FunctionDispatcher()
     {
-        for (auto &ptr : callbacks_)
+        for (auto type : types_)
         {
-            // This is ugly UB, but prevents leaks
-            delete reinterpret_cast<std::function<void(void)> *>(ptr.second);
+            type->CastAndDelete(callbacks_[type->GetTypeIndex()]);
         }
     }
 
@@ -59,7 +93,11 @@ private:
         auto previous_callback = callbacks_.find(typeid(FuncSignature));
         if (previous_callback != callbacks_.end())
         {
-            delete reinterpret_cast<std::function<typename FuncSignature::return_t(std::tuple_element_t<I, typename FuncSignature::args_t>...)> *>(previous_callback->second);
+            delete static_cast<std::function<typename FuncSignature::return_t(std::tuple_element_t<I, typename FuncSignature::args_t>...)> *>(previous_callback->second);
+        }
+        else
+        {
+            types_.push_back(new Type<FuncSignature>());
         }
         callbacks_[typeid(FuncSignature)] = (void *)(new std::function<typename FuncSignature::return_t(std::tuple_element_t<I, typename FuncSignature::args_t>...)>{std::forward<Func>(func)});
     }
@@ -69,7 +107,7 @@ private:
     {
         try
         {
-            return (*reinterpret_cast<std::function<typename FuncSignature::return_t(std::tuple_element_t<I, typename FuncSignature::args_t>...)> *>(callbacks_.at(typeid(FuncSignature))))(std::forward<Args>(args)...);
+            return (*static_cast<std::function<typename FuncSignature::return_t(std::tuple_element_t<I, typename FuncSignature::args_t>...)> *>(callbacks_.at(typeid(FuncSignature))))(std::forward<Args>(args)...);
         }
         catch (const std::out_of_range &)
         {
@@ -78,4 +116,5 @@ private:
     }
 
     std::unordered_map<std::type_index, void *> callbacks_;
+    std::vector<IType *> types_;
 };
